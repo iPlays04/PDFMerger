1c7ff533f8e3c3fc12de4a8803d9b5e81cee2b47
iPlays04 <45319429+iPlays04@users.noreply.github.com>
2025-06-18 11:03:26 +0200

    Selectionfix, Icon and design
    Added the ability to choose new files, overwriting the ones currently selected
    
    added an icon that needs to be in the same place as the code or else it crashes :)
    
    adjusted UI sizes and scaling

--- pdfmerger.py
+++ pdfmerger.py
@@ -11,33 +11,41 @@ hiclr = "#EEEFE0"
 
 def pdfChoice():
     i=0
+    
     global file_paths
+    
     file_paths.clear()
-    file_paths = filedialog.askopenfilenames(title="Select PDF files", filetypes=[("PDF files", "*.pdf")])
+    file_paths = list(filedialog.askopenfilenames(title="Select PDF files", filetypes=[("PDF files", "*.pdf")]))
+    print(file_paths)
     global boxes
     boxes=[]
+    boxes.clear()
     global boxesvars
     boxesvars=[]
+    boxesvars.clear()
     global buttons
     buttons = []
+    buttons.clear()
    
+    for widget in frPDFFrame.winfo_children():
+        widget.destroy()
 
     for file in file_paths:
         boxesvars.append(tk.IntVar(value=0))
         
-        button =tk.Button(frPDFFrame,text="up", bg=secclr, fg=bgclr, command=lambda i=i: up(i))
+        button =tk.Button(frPDFFrame,text="up", bg=secclr, command=lambda i=i: up(i))
         buttons.append(button)
         button.grid(column=0,row=i)
 
-        spinbox = tk.Spinbox(frPDFFrame, bg=secclr, fg=bgclr, width=2,from_=1, to=len(file_paths),textvariable=boxesvars[i])
+        spinbox = tk.Spinbox(frPDFFrame, bg=secclr, width=2,from_=1, to=len(file_paths),textvariable=boxesvars[i])
         boxes.append(spinbox)
         spinbox.grid(column=1,row=i)
 
-        label = tk.Label(frPDFFrame, text=file.split('/')[-1], bg=secclr, fg=bgclr)
+        label = tk.Label(frPDFFrame, text=file.split('/')[-1], bg=priclr)
         label.grid(column=2,row=i)
 
         i=i+1
-    print(boxes)
+    #print(boxes)
 
 
 def merge_pdfs():
@@ -53,7 +61,7 @@ def merge_pdfs():
         with open(pdf, 'rb') as file:
             pdf_merger.append(file)
 
-    with open("merged.pdf", 'wb') as output_file:
+    with open(f"{entDestinationentry.get()}.pdf", 'wb') as output_file:
         pdf_merger.write(output_file)
     print("PDFs merged successfully!")
 
@@ -67,14 +75,33 @@ def up(index):
 # UI
 root = tk.Tk()
 root.title("Simple PDF Merger")
-root.geometry('300x620')
+root.geometry('400x620')
 root.configure(background=bgclr)
+root.iconphoto(False, tk.PhotoImage(file='.\\icon.png'))
 
 btPdfChoice = tk.Button(root, text="PDF-Dateien wählen", bg=priclr,  command=pdfChoice, width=40)
 btPdfChoice.pack(pady=10)
 
-frPDFFrame = tk.Frame(root, bg=priclr, height=500, width=280)
-frPDFFrame.pack(pady=10)
+#frPDFMaster = tk.Frame(root, bg=priclr, height=400, width=380, highlightthickness=2, highlightbackground=hiclr, highlightcolor="white")
+#frPDFMaster.pack(pady=10, expand=False)
+
+frPDFFrame = tk.Frame(root, bg=priclr, height=400, width=380, highlightthickness=2, highlightbackground=hiclr, highlightcolor="white")
+frPDFFrame.pack(pady=10, expand=False, fill="x")
+
+#scrollbar=tk.Scrollbar(frPDFMaster,orient="vertical", bg=priclr)
+#scrollbar.pack(pady=10,side="right")
+
+labelEntry = tk.Label(root, bg=bgclr, text="Dateinamen für Merged File wählen:")
+labelEntry.pack()
+
+frEntry = tk.Frame(root)
+frEntry.pack(pady=10)
+
+entDestinationentry = tk.Entry(frEntry, bg=priclr, width=50)
+entDestinationentry.pack(side="left")
+
+labelHint = tk.Label(frEntry, bg=priclr, fg=hiclr, text=".pdf")
+labelHint.pack(side="right")
 
 btStartMerge = tk.Button(root, text="Zusammenfügen", bg=priclr,  command=merge_pdfs, width=40)
 btStartMerge.pack(pady=10)
